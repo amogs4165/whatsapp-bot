1ebfe52484469da3eff8faa43fa1269969a98366
Amogh <87270666+amogs4165@users.noreply.github.com>
2023-01-21 19:21:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
+require('dotenv').config();
+const qrcode = require('qrcode-terminal'); 
+const { Configuration, OpenAIApi } = require("openai");
+const { Client } = require('whatsapp-web.js'); 
+const client = new Client(); 
 
-  const qrcode = require('qrcode-terminal'); 
-   
-  const { Client } = require('whatsapp-web.js'); 
-  const client = new Client(); 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+  
+const openai = new OpenAIApi(configuration);
 
-  let recentContact = []
-   
+
+try {
   client.on('qr', qr => { 
     qrcode.generate(qr, { small: true }); 
   }); 
-   
+    
   client.on('ready', () => { 
     console.log('Client is ready!'); 
   }); 
-   
-  client.on('message', message => { 
-    console.log("message", message); 
-    // let content = messageContent[Math.floor(Math.random() * messageContent.length)]; 
-    let content = `Hi ${message._data.notifyName}, Amogh is currently busy please leave a message`
-    if ((message.body || message.hasMedia) && message.author === undefined && !recentContact.includes(message._data.notifyName) && message._data.notifyName) { 
-      client.sendMessage(message.from, content);
-      recentContact.push(message._data.notifyName);
-    } 
+    
+  client.on('message',  async (message) => { 
+    if(message.body && message.body.includes('@chatgpt')) {
+      const ms = message.body.substring(8);
+      const {data} =  await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: ms,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      client.sendMessage(message.from, data.choices[0].text);
+    }
   }); 
-
-  client.on('message_ack', test => {
-    console.log(test, 'message ack')
-  })
-
-  setInterval(()=> {
-      recentContact = [];
-  },900000)
-
-   
+    
   client.initialize();
+  
+} catch (error) {
+  console.error(error);
+}
